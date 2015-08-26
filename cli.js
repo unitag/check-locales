3df2fa9957b5a297e50f01163a0aafc81129b428
Cédric Connes <cedric.connes@gmail.com>
2015-08-26 11:20:46 +0200

    Use the current directory by default for the CLI

--- cli.js
+++ cli.js
@@ -1,23 +1,12 @@
 #!/usr/bin/env node
 'use strict';
 
-var path = require('path');
-
-var chalk = require('chalk');
-
 var checkLocales = require('./');
 
-if (process.argv.length < 3) {
-	console.error('Usage: ' + process.argv[0] + ' ' + path.basename(__filename) + ' <project path>');
-	process.exit(2);
-}
-
-var root = path.resolve(process.argv[2]);
-
-checkLocales(root, function (error, badBundles) {
+checkLocales(process.argv[2] || '.', function (error, badBundles) {
 	if (error) {
 		console.error('Unexpected error: ' + error);
-		process.exit(3);
+		process.exit(2);
 	}
 
 	if (badBundles.length > 0) {
